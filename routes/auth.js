574ab531c2deb3f8339b12cabbfdf21eef934f5e
aleanto <alexandar.a.todorov@gmail.com>
2024-03-07 14:36:13 +0200

    Add update user details end point

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -17,5 +18,6 @@ router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
+router.put('/updatedetails', protect, updateDetails);
 
 module.exports = router;
