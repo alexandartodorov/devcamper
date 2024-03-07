52d8820b7d4c5efe10c6c40293409a9d82607768
aleanto <alexandar.a.todorov@gmail.com>
2024-03-07 15:38:45 +0200

    Add update password end point

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
   forgotPassword,
   resetPassword,
   updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -19,5 +20,6 @@ router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
